c798fcee567f24826bb0782d283afb78a7219597
Albert Sadovnikov <albert.sadovnikov@gmail.com>
2017-04-18 22:46:50 +0300

    [WIP] More data

--- src/process.py
+++ src/process.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score
 
 """
 
@@ -19,19 +21,40 @@ def preprocess(img, cal, pt):
 
 
 def extract_features(img):
-    n_features = 3
+    n_features = 8
     hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    l, s = hls[:, :, 1], hls[:, :, 2]
     feat = np.zeros((img.shape[0], img.shape[1], n_features))
     # feature 0, lightness, scaled to 0.0 to 1.0
-    feat[:, :, 0] = hls[:, :, 1].astype(np.float) / 255
+    idx = 0
+    feat[:, :, idx] = l.astype(np.float) / 255
     # feature 1, saturation, scaled to 0.0 to 1.0
-    feat[:, :, 1] = hls[:, :, 2].astype(np.float) / 255
+    idx += 1
+    feat[:, :, idx] = s.astype(np.float) / 255
     # feature 2, y coordinate, scaled
+    idx += 1
     for row_num in range(img.shape[0]):
-        feat[row_num, :, 2] = row_num / img.shape[0]
-    # feature 3, sobel magnitude on lightness, smoothed
-    # feature 4, sobel magnitude on saturation, smoothed
-
+        feat[row_num, :, idx] = row_num / img.shape[0]
+    # feature 3, x coordinate, scaled
+    idx += 1
+    for col_num in range(img.shape[1]):
+        feat[:, col_num, 3] = col_num / img.shape[1]
+    # feature 3, Sobel x abs on lightness
+    slx = cv2.Sobel(l, cv2.CV_32F, 1, 0, ksize=5) / 255
+    idx += 1
+    feat[:, :, idx] = slx
+    # feature 4, Sobel y abs on lightness
+    sly = cv2.Sobel(l, cv2.CV_32F, 0, 1, ksize=5) / 255
+    idx += 1
+    feat[:, :, idx] = sly
+    # feature 5, Sobel x abs on saturation
+    ssx = cv2.Sobel(s, cv2.CV_32F, 1, 0, ksize=5) / 255
+    idx += 1
+    feat[:, :, idx] = ssx
+    # feature 6, Sobel y abs on saturation
+    ssy = cv2.Sobel(s, cv2.CV_32F, 0, 1, ksize=5) / 255
+    idx += 1
+    feat[:, :, idx] = ssy
     return feat
 
 if __name__ == '__main__':
@@ -40,40 +63,83 @@ if __name__ == '__main__':
     # load perspective transform data
     pt_data = np.load('data/perspective.npz')
 
-    images = [('test_images/test1.jpg', 'train_images/test1.png'),
-              ('test_images/test5.jpg', 'train_images/test5.png'),
-              ('test_images/straight_lines1.jpg', 'train_images/straight_lines1.png')]
+    images = [('train_images/test1.jpg', 'train_images/test1_mask.png'),
+              ('train_images/test5.jpg', 'train_images/test5_mask.png'),
+              ('train_images/straight_lines1.jpg', 'train_images/straight_lines1_mask.png'),
+              ('train_images/harder_challenge_0005.png', 'train_images/harder_challenge_0005_mask.png'),
+              ('train_images/harder_challenge_0009.png', 'train_images/harder_challenge_0009_mask.png'),
+              ('train_images/harder_challenge_0010.png', 'train_images/harder_challenge_0010_mask.png'),
+              ('train_images/harder_challenge_0011.png', 'train_images/harder_challenge_0011_mask.png'),
+              ('train_images/harder_challenge_0013.png', 'train_images/harder_challenge_0013_mask.png'),
+              ('train_images/harder_challenge_0014.png', 'train_images/harder_challenge_0014_mask.png')]
 
-    image_filename = images[1][0]
-    mask_filename = images[1][1]
-    # load image
-    image = preprocess(cv2.imread(image_filename), cal_data, pt_data)
-    # load mask
-    mask = preprocess(cv2.cvtColor(cv2.imread(mask_filename), cv2.COLOR_BGR2GRAY), cal_data, pt_data)
-    mask = cv2.normalize(mask.astype(np.float), None, 0.0, 1.0, cv2.NORM_MINMAX)
+    x_data = np.empty((0, 8), np.float)
+    y_data = np.empty(0, np.int)
+
+    # setup ml data
+    for image_filename, mask_filename in images:
+        # load image
+        image = preprocess(cv2.imread(image_filename), cal_data, pt_data)
+        # load mask
+        mask = preprocess(cv2.cvtColor(cv2.imread(mask_filename), cv2.COLOR_BGR2GRAY), cal_data, pt_data)
+        mask = cv2.normalize(mask.astype(np.float), None, 0.0, 1.0, cv2.NORM_MINMAX)
+
+        # cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
+        # cv2.namedWindow('Mask', cv2.WINDOW_NORMAL)
+        # cv2.imshow('Original', image)
+        # cv2.imshow('Mask', mask)
+
+        f = extract_features(image)
+        # for idx in range(f.shape[-1]):
+        #     win_name = 'Feature%d' % idx
+        #     cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+        #     imagesc(win_name, f[:, :, idx])
+        # cv2.waitKey(0)
+        # # dt = svm.LinearSVC(class_weight='balanced')
+        x_data_sample = f.reshape(-1, f.shape[-1])
+        y_data_sample = mask.flatten() > 0.5
+        x_data = np.concatenate([x_data, x_data_sample])
+        y_data = np.concatenate([y_data, y_data_sample])
+
+    print(x_data.shape)
+    print(y_data.shape)
+
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=0, stratify=y_data)
+
+    dt = DecisionTreeClassifier(max_depth=32, class_weight="balanced")
+    # dt = svm.LinearSVC(class_weight='balanced')
+    dt.fit(x_train, y_train)
+    print(dt.feature_importances_)
+
+    y_predicted = dt.predict(x_test)
+    ps = precision_score(y_test, y_predicted)
+    rs = recall_score(y_test, y_predicted)
+    print('Precision: %f, Recall: %f' % (ps, rs))
+
+    test_images = ['test_images/straight_lines2.jpg',
+                   'test_images/test2.jpg',
+                   'test_images/test3.jpg',
+                   'test_images/test4.jpg',
+                   'test_images/test6.jpg',
+                   'test_images/harder_challenge_0001.png',
+                   'test_images/harder_challenge_0002.png',
+                   'test_images/harder_challenge_0003.png',
+                   'test_images/harder_challenge_0004.png',
+                   'test_images/harder_challenge_0006.png',
+                   'test_images/harder_challenge_0007.png',
+                   'test_images/harder_challenge_0008.png',
+                   'test_images/harder_challenge_0012.png']
 
     cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
-    cv2.namedWindow('Mask', cv2.WINDOW_NORMAL)
-    cv2.imshow('Original', image)
-    cv2.imshow('Mask', mask)
+    cv2.namedWindow('Binary', cv2.WINDOW_NORMAL)
+    for test_filename in test_images:
+        image = preprocess(cv2.imread(test_filename), cal_data, pt_data)
+        f = extract_features(image)
+        res = dt.predict(f.reshape(-1, f.shape[-1]))
+        cv2.imshow('Original', image)
+        imagesc('Binary', res.reshape(image.shape[:2]))
+        cv2.waitKey(0)
 
-    f = extract_features(image)
-    for idx in range(f.shape[-1]):
-        win_name = 'Feature%d' % idx
-        cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
-        imagesc(win_name, f[:, :, idx])
-
-    # dt = DecisionTreeClassifier()
-    dt = svm.LinearSVC(class_weight='balanced')
-    x_data = f.reshape(-1, f.shape[-1])
-    y_data = mask.flatten() > 0.5
-    dt.fit(x_data, y_data)
-
-    res = dt.predict(f.reshape(-1, f.shape[-1]))
-    cv2.namedWindow('Result', cv2.WINDOW_NORMAL)
-    imagesc('Result', res.reshape(mask.shape))
-
-    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
